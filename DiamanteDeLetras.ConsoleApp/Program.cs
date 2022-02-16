9913940b39955c2efabd3cd4666c58698c26c924
Luís Eduardo Bernardino Luz <dudubluz@gmail.com>
2022-02-16 15:49:19 -0300

    Add project files.

---
+++ DiamanteDeLetras.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AtividadeDiamante.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string metade;
+
+
+            Console.ReadLine();
+        }
+    }
+}
