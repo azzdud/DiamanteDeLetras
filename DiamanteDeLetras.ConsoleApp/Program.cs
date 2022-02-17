3868003bf57f95d14a83b4cf83eb00ec8f80ab8c
Luís Eduardo Bernardino Luz <dudubluz@gmail.com>
2022-02-17 14:15:39 -0300

    Diamante de Letras Academia do Programador

--- DiamanteDeLetras.ConsoleApp/Program.cs
+++ DiamanteDeLetras.ConsoleApp/Program.cs
@@ -6,9 +6,94 @@ namespace AtividadeDiamante.ConsoleApp
     {
         static void Main(string[] args)
         {
-            string metade;
+            char letra;
+            string alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            int quantEspacosFora, quantEspacosDentro = 0, metade = 0;
+
+            Console.Write("Digite a letra que ficará no MEIO: ");
+            letra = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("");
+
+            char[] charAlfabeto = alfabeto.ToCharArray();
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+
+            for (int i = 0; i < charAlfabeto.Length; i++)
+            {
+                if (charAlfabeto[i] == letra)
+                {
+                    metade = i;
+                }
+            }
+
+            quantEspacosFora = (metade);
+
+            for (int i = 0; i <= metade; i++)
+            {
+                for (int j = 0; j < quantEspacosFora; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write(charAlfabeto[i]);
+
+                for (int k = 0; k < quantEspacosDentro; k++)
+                {
+                    Console.Write(" ");
+                }
+
+                if (i != 0)
+                {
+                    Console.Write(charAlfabeto[i]);
+                }
+                Console.WriteLine();
+
+                quantEspacosFora = quantEspacosFora - 1;
+
+                if (i == 0)
+                {
+                    quantEspacosDentro = 1;
+                }
+
+                if (i != 0 && i != metade)
+                {
+                    quantEspacosDentro = quantEspacosDentro + 2;
+                }
+            }
+
+            quantEspacosFora = 0;
+
+            for (int i = 1; i <= metade; i++)
+            {
+                quantEspacosFora = quantEspacosFora + 1;
+                if (i == metade)
+                {
+                    quantEspacosDentro = 1;
+                }
+                if (i != 0 && i != metade)
+                {
+                    quantEspacosDentro = quantEspacosDentro - 2;
+                }
 
 
+                for (int j = 0; j < quantEspacosFora; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.Write(charAlfabeto[metade - i]);
+
+                for (int k = 0; k < quantEspacosDentro; k++)
+                {
+                    Console.Write(" ");
+                }
+
+                if (i != metade)
+                {
+                    Console.Write(charAlfabeto[metade - i]);
+                }
+                Console.WriteLine();
+
+            }
             Console.ReadLine();
         }
     }
